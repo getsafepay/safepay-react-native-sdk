0c31d8614edcf66f4f7042e12a07a5ec014a3dab
Avais <avais392@gmail.com>
2022-05-25 23:55:35 +0500

    moved the fetchToken function out of the useEffect callback

--- src/components/Checkout.tsx
+++ src/components/Checkout.tsx
@@ -23,8 +23,7 @@ const SafepayCheckout: React.FC<SafepayCheckoutProps> = (
 
   const [modalVisible, setModalVisible] = useState(false);
   const [token, setToken] = useState('');
-  useEffect(() => {
-    const fetchToken = async () => {
+   const fetchToken = async () => {
       try {
         const response = await fetch(`${baseURL}order/v1/init`, {
           method: 'POST',
@@ -46,6 +45,8 @@ const SafepayCheckout: React.FC<SafepayCheckoutProps> = (
         console.error(error);
       }
     };
+    
+    useEffect(() => {
     if (modalVisible) {
       fetchToken();
     }
